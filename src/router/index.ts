d5fe170a15cd5a6fe599a1a2faeefd6feb27cb97
Ngoc Tuan <tuansaker1412@gmail.com>
2024-03-11 14:08:59 +0700

    init project

---
+++ src/router/index.ts
@@ -0,0 +1,31 @@
+import { createRouter, createWebHistory } from 'vue-router'
+import HomeView from '../views/HomeView.vue'
+import NotFound from '../pages/NotFound.vue'
+
+const router = createRouter({
+  history: createWebHistory(import.meta.env.BASE_URL),
+  routes: [
+    // will match everything and put it under `$route.params.pathMatch`
+    { path: '/:pathMatch(.*)*', name: 'NotFound', component: NotFound },
+    {
+      path: '/',
+      name: 'home',
+      component: HomeView
+    },
+    {
+      path: '/about',
+      name: 'about',
+      // route level code-splitting
+      // this generates a separate chunk (About.[hash].js) for this route
+      // which is lazy-loaded when the route is visited.
+      component: () => import('../views/AboutView.vue')
+    },
+    {
+      path: '/sign-up',
+      name: 'sign-up',
+      component: () => import('../views/SignUpView.vue')
+    },
+  ]
+})
+
+export default router
---
+++ src/views/HomeView.vue
@@ -0,0 +1,17 @@
+<script setup lang="ts">
+</script>
+
+<template>
+  <main>
+    <div class="container mx-auto">
+      <h1 className="text-3xl font-bold underline">
+        Hello world!
+      </h1>
+      <div class="flex flex-row">
+        <div class="basis-1/4">01</div>
+        <div class="basis-1/4">02</div>
+        <div class="basis-1/2">03</div>
+      </div>
+    </div>
+  </main>
+</template>
